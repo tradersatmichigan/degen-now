ba220b67e7b96d4e30372ba62aa743a9c2dda9ee
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-06-07 08:49:42 -0700

    skeleton

---
+++ backend/handlers.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func handleCreateTable(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleJoinTable(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleTableAction(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleServeEvents(w http.ResponseWriter, r *http.Request) {
+
+}
