11ee9fa95c6d127012f6decd6b4f9c9670530bc6
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-27 23:56:29 -0700

    idk im tired

---
+++ backend/src/handler.rs
@@ -0,0 +1,11 @@
+enum DegenError {
+}
+
+impl poem::IntoResponse for DegenError {
+    fn into_response(self) -> poem::Response {
+        todo!()
+    }
+}
+
+mod create_game {
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
+use poem::{listener::TcpListener, Route, Server};
 
+mod handler;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new();
+
+    Server::new(TcpListener::bind("0.0.0.0:3000"))
+        .run(app)
+        .await
 }
