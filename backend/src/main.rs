9d8c849e169d20e62eb918adb2ebd430b7378a02
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-28 07:56:00 -0700

    lmao

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,4 @@
-use poem::{listener::TcpListener, Route, Server};
-
-mod handler;
-
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    let app = Route::new();
+async fn main() {
 
-    Server::new(TcpListener::bind("0.0.0.0:3000"))
-        .run(app)
-        .await
 }
