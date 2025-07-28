783a3c3b728eebb72ea9543a2981c39ab9cc0676
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-07-28 08:07:45 -0700

    app error

---
+++ backend/src/handler.rs
@@ -0,0 +1,15 @@
+use axum::{http::StatusCode, response::IntoResponse};
+
+struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+    }
+}
+
+impl<E: Into<anyhow::Error>> From<E> for AppError {
+    fn from(value: E) -> Self {
+        Self(value.into())
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,13 @@
+use axum::Router;
+
+mod handler;
+
 #[tokio::main]
 async fn main() {
 
+    // Add api routes here
+    let app = Router::new();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
